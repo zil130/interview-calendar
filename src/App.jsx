3b30747e7b6cee6b61b704474178c8b547ab2703
zil130 <55101149+zil130@users.noreply.github.com>
2023-05-17 14:50:01 -0300

    refactor: AppContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import Container from './components/Container';
+import { styled } from 'styled-components';
 import Header from './components/Header';
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 740px;
+  height: 100vh;
+  margin: 0 auto;
+  background: var(--bg-smoke);
+  border: 1px solid var(--bg-smoke);
+`;
 
 function App() {
   return (
-    <Container>
+    <AppContainer>
       <Header />
-    </Container>
+    </AppContainer>
   );
 }
 
